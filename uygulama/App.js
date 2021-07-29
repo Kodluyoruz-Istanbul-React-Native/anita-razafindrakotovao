640b476c534e993b091038ca52734036778eeaa5
anitachana <anitachana.r@gmail.com>
2021-07-29 23:23:52 +0300

    Hello world

---
+++ uygulama/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import {View, Text} from "react-native";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+  }
+  componentDidMount(){
+
+  }
+  componentDidUpdate(prevProps, prevState){
+
+  }
+  componentWillUnmount(){
+
+  }
+  render(){
+    return (
+      <View>
+        <Text>
+          Hello world.
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
